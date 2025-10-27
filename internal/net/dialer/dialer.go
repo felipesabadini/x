3068409dc641a9f8a1345c4bd09e62c07f460d71
Felipe Sabadini Facina <fsabadini@hotmail.com>
2025-10-27 18:23:58 -0300

    "Add debug logging for network interface bindings and ensure ephemeral port assignment when binding to an interface."

--- internal/net/dialer/dialer.go
+++ internal/net/dialer/dialer.go
@@ -157,20 +157,31 @@ func (d *Dialer) dialOnce(ctx context.Context, network, addr, ifceName string, i
 
 	var localAddr net.Addr
 	if ifAddr != nil && ifceName != "" {
+		// When binding to an interface, we need to use the IP from ifAddr
+		// but let the system choose an ephemeral port (port 0).
+		// The Control function below will handle binding to the interface.
 		switch addr := ifAddr.(type) {
 		case *net.TCPAddr:
 			localAddr = &net.TCPAddr{
-				IP: addr.IP,
+				IP:   addr.IP,
+				Port: 0, // Let system choose ephemeral port
+				Zone: addr.Zone,
 			}
+			log.Debugf("Binding to interface %s with IP %s (zone: %s)", ifceName, addr.IP, addr.Zone)
 		case *net.UDPAddr:
 			localAddr = &net.UDPAddr{
-				IP: addr.IP,
+				IP:   addr.IP,
+				Port: 0, // Let system choose ephemeral port
+				Zone: addr.Zone,
 			}
+			log.Debugf("Binding to interface %s with IP %s (zone: %s)", ifceName, addr.IP, addr.Zone)
 		default:
 			localAddr = ifAddr
+			log.Debugf("Using ifAddr as-is for interface %s: %s (type: %T)", ifceName, ifAddr, ifAddr)
 		}
 	} else {
 		localAddr = ifAddr
+		log.Debugf("No interface binding (ifAddr: %v, ifceName: %s)", localAddr, ifceName)
 	}
 
 	netd := net.Dialer{
